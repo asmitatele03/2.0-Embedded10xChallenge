d27d5fb176b8d9bb4bd81b1c8e122ceab8f49bf7
Asmita Tele <asmitatele03@gmail.com>
2026-01-28 21:35:09 +0530

    Update code.c

--- Day2.1_LCD_string/code.c
+++ Day2.1_LCD_string/code.c
@@ -1,1 +1,20 @@
+#include <lpc21xx.h>
+#include "header.h"
+
+int main()
+{
+    int i;
+    lcd_init();
+
+    while(1)
+    {
+        for(i = 0; i < 16; i++)
+        {
+            lcd_cmd(0x80 + i);
+            lcd_string("good afternoon");
+            delay_ms(200);
+            lcd_cmd(0x01);
+        }
+    }
+}
 
