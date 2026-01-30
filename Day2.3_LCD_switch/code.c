703836f5b28eaa0663c7e9fc7bcd740867f7aec4
Asmita Tele <asmitatele03@gmail.com>
2026-01-30 16:10:28 +0530

    Update code.c

--- Day2.3_LCD_switch/code.c
+++ Day2.3_LCD_switch/code.c
@@ -1,1 +1,28 @@
+#include<lpc21xx.h>
+#include"header.h"
+#define sw !((IOPIN0>>14)&1)
+
+int main()
+{
+    int c = 0;
+    lcd_init();
+
+    while(1)
+    {
+        lcd_cmd(0x80);
+        lcd_data((c/10)+48);
+        lcd_data((c%10)+48);
+        delay_ms(200);
+        lcd_cmd(0x01);
+
+        if(sw == 0)
+        {
+            while(sw == 0);
+            c++;
+
+            if(c >= 10)
+                c = 0;
+        }
+    }
+}
 
