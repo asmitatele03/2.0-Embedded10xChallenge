fef0b3d0d9e382cca2820b3336842572d8466b47
Asmita Tele <asmitatele03@gmail.com>
2026-01-31 21:05:36 +0530

    Update code.c

--- Day2.4_LCD_CGRAM/code.c
+++ Day2.4_LCD_CGRAM/code.c
@@ -1,1 +1,11 @@
+#include <LPC214x.h>
+#include "header.h"
+
+int main()
+{
+    lcd_init();      // Initialize LCD
+    lcd_cgram();     // Load custom character pattern into CGRAM
+    lcd_cmd(0x82);   // Place cursor at specific DDRAM address
+    lcd_data(1);     // Display custom character stored at CGRAM location 1
+}
 
