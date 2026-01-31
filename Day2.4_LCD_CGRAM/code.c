bbe39f8a7531cd573b15a67795ad135e786f4b51
Asmita Tele <asmitatele03@gmail.com>
2026-01-31 21:09:09 +0530

    Update code.c

--- Day2.4_LCD_CGRAM/code.c
+++ Day2.4_LCD_CGRAM/code.c
@@ -6,6 +6,6 @@ int main()
     lcd_init();      // Initialize LCD
     lcd_cgram();     // Load custom character pattern into CGRAM
     lcd_cmd(0x82);   // Place cursor at specific DDRAM address
-    lcd_data(1);     // Display custom character stored at CGRAM location 1
+    lcd_data(1);     // Display custom character stored at CGRAM location 1 (page 1)
 }
 
