151b74774bceae659face6bc41705fb3bf559807
Asmita Tele <asmitatele03@gmail.com>
2026-02-01 16:26:17 +0530

    Update code.c

--- Day2.5_LED/code.c
+++ Day2.5_LED/code.c
@@ -1,1 +1,31 @@
+#include <lpc21xx.h>
+#include "header.h"
+
+#define LED 255
+
+int main()
+{
+    int i;
+
+    IODIR0 = LED;
+    IOSET0 = LED;
+    delay_ms(20);
+
+    while(1)
+    {
+        for(i = 0; i < 8; i = i + 2)
+        {
+            if((i == 0) || (i == 2))    // Based on your image logic
+            {
+                IOCLR0 = 1 << i;       // Turn LED ON
+                delay_ms(20);
+                IOSET0 = 1 << i;       // Turn LED OFF
+                delay_ms(20);
+            }
+        }
+
+        if(i == 6)
+            i = i - 1;
+    }
+}
 
