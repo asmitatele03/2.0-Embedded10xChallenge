94636c3259712eb68b12774cf813e576b18cc312
Asmita Tele <asmitatele03@gmail.com>
2026-02-02 21:59:59 +0530

    Update code.c

--- Day2.6_LED/code.c
+++ Day2.6_LED/code.c
@@ -1,1 +1,40 @@
+#include <lpc21xx.h>
+
+void delay_ms(unsigned int ms)
+{
+    unsigned int i, j;
+    for(i = 0; i < ms; i++)
+        for(j = 0; j < 6000; j++);
+}
+
+int main()
+{
+    IO1DIR = 0xFF;     // P1.0 – P1.7 as output
+
+    while(1)
+    {
+        // ---- BLINK ALL LEDs 5 TIMES ----
+        for(int i = 0; i < 5; i++)
+        {
+            IO1CLR = 0xFF;   // All LEDs ON
+            delay_ms(100);
+
+            IO1SET = 0xFF;   // All LEDs OFF
+            delay_ms(100);
+        }
+
+        // ---- LEFT TO RIGHT SHIFT 5 TIMES ----
+        for(int i = 0; i < 5; i++)
+        {
+            for(int j = 0; j < 8; j++)
+            {
+                IO1CLR = (1 << j);  // Turn ON LED at position j
+                delay_ms(100);
+
+                IO1SET = (1 << j);  // Turn OFF LED at position j
+                delay_ms(100);
+            }
+        }
+    }
+}
 
