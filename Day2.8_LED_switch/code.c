7fe33a8c076705910631d271c5ad18dd350cf39e
Asmita Tele <asmitatele03@gmail.com>
2026-02-04 21:39:54 +0530

    Update code.c

--- Day2.8_LED_switch/code.c
+++ Day2.8_LED_switch/code.c
@@ -1,1 +1,19 @@
+#include <lpc214x.h>
+
+void delay(int d) { for (volatile int i = 0; i < d; i++); }
+
+int main() {
+    IODIR0 |= (1<<17);
+    IODIR0 &= ~(1<<14);
+
+    int mode = 0;
+    int speed[] = {900000, 500000, 150000};
+
+    while (1) {
+        if (!(IOPIN0 & (1<<14))) { mode = (mode + 1) % 3; delay(300000); }
+
+        IOSET0 = (1<<17); delay(speed[mode]);
+        IOCLR0 = (1<<17); delay(speed[mode]);
+    }
+}
 
