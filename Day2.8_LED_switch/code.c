8634e44a4342b2b48c726c37d496cbf873bb0cf8
Asmita Tele <asmitatele03@gmail.com>
2026-02-04 22:50:04 +0530

    Update code.c

--- Day2.8_LED_switch/code.c
+++ Day2.8_LED_switch/code.c
@@ -1,6 +1,7 @@
 #include <lpc21xx.h>
 
-void delay(int d) { for (volatile int i = 0; i < d; i++); }
+void delay(int d) { 
+for (volatile int i = 0; i < d; i++); }
 
 int main() {
     IODIR0 |= (1<<17);
