cb94852e0e65304444b8cc12ccb3b25d1f0487c2
Asmita Tele <asmitatele03@gmail.com>
2026-02-12 18:28:45 +0530

    Update code.c

--- Day2.9_LED/code.c
+++ Day2.9_LED/code.c
@@ -1,1 +1,47 @@
+#include <lpc21xx.h>
+#include"header.h"
+
+#define SW (1<<14)
+
+int main()
+{
+    unsigned int led = 1;
+    int direction = 1;      
+    unsigned char last = 1;
+
+    IO0DIR |= 0xFF;         
+    IO0DIR &= ~SW;          
+
+    while(1)
+    {
+        /* -------- SWITCH EDGE DETECTION -------- */
+        if(!(IO0PIN & SW) && last)
+        {
+            direction = -direction;  
+            delay_ms(10);            // debounce 50ms
+        }
+        last = (IO0PIN & SW);
+
+        /* -------- LED OUTPUT -------- */
+        IO0CLR = 0xFF;
+        IO0SET = led;
+
+        /* -------- 1 SECOND DELAY -------- */
+        delay_ms(10);     // ? 1 second delay
+
+        /* -------- SHIFT LOGIC -------- */
+        if(direction == 1)
+        {
+            led <<= 1;
+            if(led > 0x80)
+                led = 0x01;
+        }
+        else
+        {
+            led >>= 1;
+            if(led < 0x01)
+                led = 0x80;
+        }
+    }
+}
 
